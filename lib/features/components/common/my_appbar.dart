6ebed60b1d2df6953defea139db7a8c6339480a7
AbrarAbe <abrar.abe01@gmail.com>
2025-04-04 10:48:02 +0700

    feat: accept an optional list of actions in AppBar

--- lib/features/components/common/my_appbar.dart
+++ lib/features/components/common/my_appbar.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class MyAppbar extends StatelessWidget implements PreferredSizeWidget {
   final Text title;
-  const MyAppbar({super.key, required this.title});
+  List<Widget>? actions;
+  MyAppbar({super.key, required this.title, this.actions});
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(title: title);
+    return AppBar(title: title, actions: actions);
   }
 
   @override
