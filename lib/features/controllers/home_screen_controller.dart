e91a923e25508c0dac7799822c3ca6d4c99e0792
AbrarAbe <abrar.abe01@gmail.com>
2025-04-04 10:50:04 +0700

    refactor: move logout logic to HomeScreenController and simplify SettingsScreen
    This commit refactors the SettingsScreen and HomeScreenController to consolidate logout functionality within the controller.
    
    - The SettingsScreen is now a StatelessWidget that displays user information and static content. The logout button and its associated logic have been removed.
    - The HomeScreenController now handles the logout action, including displaying a loading indicator and a snackbar upon successful logout. The logout button is conditionally displayed in the AppBar when the Settings screen is active.

--- lib/features/controllers/home_screen_controller.dart
+++ lib/features/controllers/home_screen_controller.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../core/repositories/auth_repository.dart';
+import '../../core/services/supabase_service.dart';
 import '../components/common/my_appbar.dart';
 import '../components/common/bottom_navigation_bar.dart'; // Import the bottom navigation bar component
 import '../screens/home/home_screen.dart';
@@ -37,10 +41,56 @@ class _HomeScreenControllerState extends ConsumerState<HomeScreenController> {
         : "Settings";
   }
 
+  ScaffoldFeatureController<SnackBar, SnackBarClosedReason> _snackbar(
+    BuildContext context,
+  ) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('See you later!'),
+        behavior: SnackBarBehavior.floating,
+        margin: const EdgeInsets.all(25),
+        showCloseIcon: true,
+        elevation: 1,
+        duration: const Duration(seconds: 3),
+        dismissDirection: DismissDirection.horizontal,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        backgroundColor: Colors.blueAccent,
+      ),
+    );
+  }
+
+  Future<dynamic> _loading(BuildContext context) {
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => const Center(child: CircularProgressIndicator()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final authRepository = AuthRepository(SupabaseService.client);
     return Scaffold(
-      appBar: MyAppbar(title: Text(_title())),
+      appBar: MyAppbar(
+        title: Text(_title()),
+        actions:
+            _title() == "Settings"
+                ? [
+                  IconButton(
+                    icon: const Icon(Icons.logout),
+                    onPressed: () async {
+                      // Show loading indicator
+                      _loading(context);
+                      // Sign out and Navigate to login screen
+                      await authRepository.signOut();
+                      context.go('/');
+                      // Show snackbar
+                      _snackbar(context);
+                    },
+                  ),
+                ]
+                : null,
+      ),
       body: _widgetOptions.elementAt(_selectedIndex),
       bottomNavigationBar: HomeBottomNavigationBar(
         // Use the imported component
--- lib/features/screens/home/settings_screen.dart
+++ lib/features/screens/home/settings_screen.dart
@@ -1,40 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:supabase_auth/core/repositories/auth_repository.dart';
-import 'package:go_router/go_router.dart';
+
+import '../../../core/repositories/auth_repository.dart';
 import '../../../core/services/supabase_service.dart';
 
-class SettingsScreen extends ConsumerWidget {
+class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
-  ScaffoldFeatureController<SnackBar, SnackBarClosedReason> _snackbar(
-    BuildContext context,
-  ) {
-    return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text('See you later!'),
-        behavior: SnackBarBehavior.floating,
-        margin: const EdgeInsets.all(25),
-        showCloseIcon: true,
-        elevation: 1,
-        duration: const Duration(seconds: 3),
-        dismissDirection: DismissDirection.horizontal,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        backgroundColor: Colors.blueAccent,
-      ),
-    );
-  }
-
-  Future<dynamic> _loading(BuildContext context) {
-    return showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) => const Center(child: CircularProgressIndicator()),
-    );
-  }
-
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final authRepository = AuthRepository(SupabaseService.client);
     final user = authRepository.currentUser;
     return Scaffold(
@@ -42,29 +15,8 @@ class SettingsScreen extends ConsumerWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Settings Screen Content'),
             Text('You logged in as ${user?.email ?? 'User'}'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                    Theme.of(context).buttonTheme.colorScheme?.primary,
-                foregroundColor:
-                    Theme.of(context).buttonTheme.colorScheme?.onPrimary,
-              ),
-              child: const Text('Logout'),
-              onPressed: () async {
-                // Show loading indicator
-                _loading(context);
-
-                // Sign out and Navigate to login screen
-                await authRepository.signOut();
-                context.go('/');
-
-                // Show snackbar
-                _snackbar(context);
-              },
-            ),
+            const Text('Settings Screen Content'),
           ],
         ),
       ),
