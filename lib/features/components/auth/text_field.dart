f4a8d6c4255aedaf185ba4fe468aa76483287cae
AbrarAbe <abrar.abe01@gmail.com>
2025-04-06 22:34:28 +0700

    feat: add label, icons, and hint text to MyTextField

--- lib/features/components/auth/text_field.dart
+++ lib/features/components/auth/text_field.dart
@@ -3,14 +3,24 @@
 import 'package:flutter/material.dart';
 
 class MyTextField extends StatelessWidget {
-  final String hintText;
+  final String label;
+  final String? hintText;
   final bool obscureText;
+  final Icon? suffixIcon;
+  final Color? suffixIconColor;
+  final Icon? prefixIcon;
+  final Color? prefixIconColor;
   final TextEditingController? controller;
 
   const MyTextField({
     super.key,
-    required this.hintText,
-    required this.obscureText,
+    required this.label,
+    this.hintText,
+    this.suffixIcon,
+    this.suffixIconColor,
+    this.prefixIcon,
+    this.prefixIconColor,
+    this.obscureText = false,
     this.controller,
   });
 
@@ -21,6 +31,18 @@ class MyTextField extends StatelessWidget {
       controller: controller,
       style: TextStyle(color: Theme.of(context).colorScheme.primary),
       decoration: InputDecoration(
+        suffixIcon: suffixIcon,
+        suffixIconColor: suffixIconColor,
+        labelText: label,
+        labelStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
+        hintText: hintText,
+        hintStyle: TextStyle(
+          color: Theme.of(context).colorScheme.onSurface.withAlpha(50),
+        ),
+        prefixIcon: prefixIcon,
+        prefixIconColor: prefixIconColor,
+        fillColor: Theme.of(context).colorScheme.surface,
+        filled: true,
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(12),
           borderSide: BorderSide(
@@ -33,8 +55,6 @@ class MyTextField extends StatelessWidget {
             color: Theme.of(context).colorScheme.secondary,
           ),
         ),
-        hintText: hintText,
-        hintStyle: TextStyle(color: Theme.of(context).colorScheme.onSurface),
       ),
     );
   }
